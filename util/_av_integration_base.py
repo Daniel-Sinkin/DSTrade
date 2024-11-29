c153aa617039d582a59a2c2b373d785d405e044e
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-11-29 18:18:31 +0100

    Fully implemented documentation -> integration script

---
+++ util/_av_integration_base.py
@@ -0,0 +1,115 @@
+import datetime as dt
+import json
+import logging
+import os
+from typing import Literal, Optional
+
+import requests
+from dotenv import load_dotenv
+
+api_logger = logging.Logger("APIHandler")
+api_logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+stream_handler.setFormatter(formatter)
+
+api_logger.addHandler(stream_handler)
+
+load_dotenv()
+API_KEY_ALPHAVANTAGE = os.getenv("API_KEY_ALPHAVANTAGE")
+if API_KEY_ALPHAVANTAGE is None:
+    raise RuntimeError(
+        "Couldn't find the Alphavantage api key in the environment variables!"
+    )
+
+
+def obfuscate_api_key(api_key: str) -> str:
+    if api_key == "demo":  # Don't have to obfuscate demo account
+        return api_key
+
+    return api_key[:2] + "..." + api_key[-2:]
+
+
+def obfuscate_request_url(request_url: str, api_key: str) -> str:
+    first_part = request_url.split("&apikey=")[0]
+    return first_part + f"&apikey={obfuscate_api_key(api_key)}"
+
+
+def get_utc_timestamp_ms() -> int:
+    return int(dt.datetime.now(tz=dt.timezone.utc).timestamp() * 1000)
+
+
+class AlphaVantageAPIHandler:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.url_base = "https://www.alphavantage.co/"
+        self.url_request = self.url_base + "query?"
+
+        self.logger = api_logger
+
+        self.logger.debug(f"Created {self}")
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def __repr__(self) -> str:
+        return f"AlphaVantageAPIHandler(api_key={obfuscate_api_key(self.api_key)})"
+
+    def send_request(
+        self,
+        function: str,
+        request_args: list[str],
+        save_result: bool = True,
+    ) -> Optional[dict[str, any] | list[dict[str, any]]]:
+        """
+        The key where the data is, is rather inconsitent in the API, if data_key
+        is not set then we first check if there is only one key, then that one has to be
+        the data key, otherwise we check for a list of known non-data keys and return the
+        other keys' value (not guaranteed to be correct until everything is implemented)
+
+        Best practice is to just pass it.
+        """
+        if 'datatype="csv"' in request_args:
+            raise NotImplementedError("Currently only JSON is supported!")
+
+        request_url = self.url_request + "&".join(
+            [f"function={function}"] + request_args + [f"apikey={self.api_key}"]
+        )
+
+        try:
+            response = requests.get(request_url)
+        except Exception as e:
+            self.logger.error(f"Request got generic error '{e}'")
+            return None
+        response_data: dict[str, any] = response.json()
+
+        if save_result:
+            filename = (
+                f"{get_utc_timestamp_ms()}"
+                + "__"
+                + "&".join(["function"] + request_args)
+            )
+            with open(f"saved_responses/{filename}.json", "w") as file:
+                json.dump(response_data, file)
+
+        if "Information" in response_data:
+            assert len(response_data) == 1
+            self.logger.warning(
+                f"Got Information as reponse: '{response_data['Information']}'"
+            )
+            return None
+        if "Error Message" in response_data:
+            assert len(response_data) == 1, "'Error Message' key but also other keys!"
+            self.logger.warning(
+                f"Got the following error response: {response_data['Error Message']}."
+            )
+            return None
+
+        self.logger.debug(
+            f"Successfuly sent request '{obfuscate_request_url(request_url, self.api_key)}'"
+        )
+
+        return response_data
---
+++ util/av_doc_to_integration.py
@@ -0,0 +1,121 @@
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the Alpha Vantage documentation
+url = "https://www.alphavantage.co/documentation/"
+
+
+def main() -> None:
+    # Fetch the page content
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+    else:
+        print(f"Failed to fetch the page. Status code: {response.status_code}")
+        raise RuntimeError
+
+    main_content = soup.find("article")
+
+    split = [
+        line
+        for line in main_content.text.splitlines()
+        if (
+            "❚ Required" in line
+            and "❚ Required: function" not in line
+            and "❚ Required: apikey" not in line
+        )
+        or "❚ Optional" in line
+        or " function=" in line
+    ]
+
+    # Initialize variables
+    grouped_dict = {}  # This will be the final dictionary
+    current_group = {"required": [], "optional": []}
+    current_function = None
+
+    # Iterate through the list
+    for item in split:
+        if "function=" in item:
+            # When a new function is found, save the current group under the previous function name
+            if current_function and (
+                current_group["required"] or current_group["optional"]
+            ):
+                grouped_dict[current_function] = current_group
+                current_group = {"required": [], "optional": []}
+            # Extract the new function name
+            current_function = item.split("function=")[-1].strip()
+        elif item.startswith("❚"):
+            if "Required:" in item:
+                param = item.replace(" ", "").replace("❚Required:", "").strip()
+                if param not in current_group["required"]:
+                    current_group["required"].append(param)
+            elif "Optional:" in item:
+                param = item.replace(" ", "").replace("❚Optional:", "").strip()
+                if param != "datatype" and param not in current_group["optional"]:
+                    current_group["optional"].append(param)
+
+    # After the loop, save the last group under its function name
+    if current_function and (current_group["required"] or current_group["optional"]):
+        grouped_dict[current_function] = current_group
+
+    brace_L = "{"
+    brace_R = "}"
+    iteration = 0
+
+    av_integration_py = "from typing import Optional, Literal\n"
+    with open(Path("util").joinpath("_av_integration_base.py"), "r") as file:
+        av_integration_py = file.read()
+
+    for k, v in grouped_dict.items():
+        iteration += 1
+        args_required = ""
+        req_args_required = ""
+        for i, arg in enumerate(v["required"]):
+            args_required += f"    {arg},"
+            req_args_required += f'            f"{arg}={brace_L}{arg}{brace_R}",'
+
+            if i < len(v["required"]) - 1:
+                args_required += "\n"
+                req_args_required += "\n"
+
+        args_optional = ""
+        req_args_optional = ""
+        for j, arg in enumerate(v["optional"]):
+            args_optional += f"    {arg}=None,"
+            f_string = f'f"{arg}={brace_L}{arg}{brace_R}"'
+            req_args_optional += (
+                f"            + ([{f_string}] if {arg} is None else [])"
+            )
+            if j < len(v["optional"]) - 1:
+                args_optional += "\n"
+                req_args_optional += "\n"
+
+        args_required = f"""
+    def get_{k.lower()}(
+        self,
+    {args_required}
+    {args_optional}
+        datatype: Literal["json", "csv"] = "json",
+        **kwargs
+    ) -> Optional[dict[str, any]]:
+        return self.send_request(
+            function=\"{k}\",
+            request_args=[
+    {req_args_required}
+    {req_args_optional}
+            ],
+            **kwargs
+        )
+    """
+        av_integration_py += args_required
+
+    with open(Path("src").joinpath("av_integration.py"), "w") as file:
+        file.write(av_integration_py)
+
+
+if __name__ == "__main__":
+    main()
