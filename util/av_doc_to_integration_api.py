94479be0a6e68aa207413bb6d5780ebd652f63cd
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-12-14 11:50:05 +0100

    Fully implemented the example integration, migrated the new code and almost ready to do the setup for the generated code, only real thing missing is generating examples out of the collected entries

--- util/av_doc_to_integration_api.py
+++ util/av_doc_to_integration_api.py
@@ -9,75 +9,119 @@ RBRACE = "}"
 
 url = "https://www.alphavantage.co/documentation/"
 
+response = requests.get(url)
+if response.status_code != 200:
+    raise RuntimeError("Failed to get HTML documentation from AlphaVantage!")
+
+soup = bs4.BeautifulSoup(response.text, "html.parser")
+
 
 def process_section(
     section: bs4.element.Tag,
 ) -> tuple[str, dict[str, dict[str, str | list[str]]]]:
-    section_title = section.find("h2").text
-    # print("Section Title:", section_title)
+    """
+    Processes the section, returning a tuple consisting of the section name and a dict with
+    keys
 
-    collection = {}
+    * "description"
+    * "id"
+    * "examples"
+    * "args_required"
+    * "args_optional"
+
+    for each API function in that section
+    """
+    section_title = section.find("h2").text
+
+    function_map = {}
     contents = [
         c for c in section.contents if c != "\n" and not str(c).startswith("<br/")
     ]
-    i = 0
-    while True:
+    line_count = 0
+    for _ in range(1000):
         try:
-            while not str(contents[i]).startswith("<h4"):
-                i += 1
+            while not str(contents[line_count]).startswith("<h4"):
+                line_count += 1
         except IndexError:
             break
-        func_name = contents[i].text
-        id_ = contents[i].get("id")
+        func_name = contents[line_count].text
+        id_ = contents[line_count].get("id")
         if func_name.startswith("Quote Endpoint"):
             func_name = "GLOBAL_QUOTE"
 
-        i += 1
+        line_count += 1
         descr = []
-        while str(contents[i]).startswith("<p"):
-            if contents[i].text != "":
-                descr.append(contents[i].text.strip())
-            i += 1
+        while str(contents[line_count]).startswith("<p"):
+            if contents[line_count].text != "":
+                descr.append(contents[line_count].text.strip())
+            line_count += 1
         description = "\n".join(descr)
 
-        assert str(contents[i]) == "<h6><b>API Parameters</b></h6>"
-        i += 1
+        assert str(contents[line_count]) == "<h6><b>API Parameters</b></h6>"
+        line_count += 1
         reqs = []
         opts = []
-        while True:
-            if str(contents[i]) == "<p><b>❚ Required: <code>apikey</code></b></p>":
-                i += 2
+        for _ in range(100):
+            if (
+                str(contents[line_count])
+                == "<p><b>❚ Required: <code>apikey</code></b></p>"
+            ):
+                line_count += 2
                 break
-            argument = str(contents[i].find("code").text)
+            argument = str(contents[line_count].find("code").text)
             if argument == "function":
-                code = contents[i + 1].find("code")
+                code = contents[line_count + 1].find("code")
                 if code is not None:
                     func_name = code.text.split("=")[1]
-                i += 2
+                line_count += 2
                 continue
 
             lines = []
-            is_req = str(contents[i]).startswith("<p><b>❚ Required: ")
-            i += 1
-            while "❚" not in str(contents[i]):
-                lines.append(contents[i].text)
-                i += 1
+            is_req = str(contents[line_count]).startswith("<p><b>❚ Required: ")
+            line_count += 1
+            while "❚" not in str(contents[line_count]):
+                lines.append(contents[line_count].text.strip())
+                line_count += 1
 
             annotated_content = [argument, "\n".join(lines)]
             if is_req:
                 reqs.append(annotated_content)
             else:
                 opts.append(annotated_content)
+        else:
+            raise RuntimeError("Infinite Inner Section Loop Detected")
+
+        assert contents[line_count].text.startswith("Example")
+        line_count += 1
+
+        examples = []
+        for _ in range(100):
+            if contents[line_count].text != "Language-specific guides":
+                examples.append(contents[line_count].text)
+                line_count += 1
+            else:
+                break
+        else:
+            raise RuntimeError("Infinite Inner section 2 loop detected.")
+        if len(examples) > 0:
+            examples = examples[:-1]
+
+        assert contents[line_count].text == "Language-specific guides"
+        line_count += 1
+        assert str(contents[line_count]).startswith("<div>\n<button class=")
 
         assert func_name is not None
-        collection[func_name] = {
+        function_map[func_name] = {
             "description": description,
             "id": id_,
+            "examples": examples,
             "args_required": reqs,
             "args_optional": opts,
         }
         func_name = None
-    return section_title, collection
+    else:
+        raise RuntimeError("Infinite Outer Section Loop Detected")
+    return section_title, function_map
 
 
 def format_opt_request_arg(arg: str) -> str:
@@ -88,7 +132,68 @@ def format_opt_arg(arg: str) -> str:
     return f"{arg}:Optional[any]=None"
 
 
-def write_processed_dict_to_script(section_dict) -> None:
+def print_function(k: str, v: dict[str, any]) -> None:
+    args_req = [a[0] for a in v["args_required"]]
+    args_req_str = ", ".join(args_req)
+    args_req_request = [f'"{arg}={arg}"' for arg in args_req]
+
+    args_opt = [a[0] for a in v["args_optional"]]
+    args_opt_adj = [format_opt_arg(arg) for arg in args_opt]
+    args_opt_str = ", ".join(args_opt_adj)
+    args_opt_request = [format_opt_request_arg(arg) for arg in args_opt]
+
+    args = ["self"]
+    if args_req_str != "":
+        args.append(args_req_str)
+    if args_opt_str != "":
+        args.append(args_opt_str)
+    print(
+        "    ",
+        f"def get_{k.lower()}({','.join(args)},**kwargs) -> dict[str, any]:",
+        sep="",
+    )
+    print('        """')
+    print(f"https://www.alphavantage.co/documentation/#{v['id']}")
+    for line_count in v["description"].splitlines():
+        print("", line_count, sep="")
+    for arg, desc in v["args_required"]:
+        print(f"### {arg} (required)")
+        print(f"{desc}")
+    for arg, desc in v["args_optional"]:
+        print(f"### {arg} (optional)")
+        print(f"{desc}")
+    print('        """')
+    request_args_optional = (
+        f" + {' + '.join(args_opt_request)}" if len(args_opt_request) > 0 else ""
+    )
+    print(f"""
+return self._send_request(
+    function="{k}",
+    request_args=[{','.join(args_req_request)}]{request_args_optional},
+    **kwargs
+)
+    """)
+
+
+def print_section(section: str, section_dict: dict[str, dict[str, any]]) -> None:
+    print("    ", "#" * (len(section) + 4), sep="")
+    print("    ", "# ", section, " #", sep="")
+    print("    ", "#" * (len(section) + 4), sep="")
+    print()
+    for func_name, func_dict in section_dict.items():
+        print_function(func_name, func_dict)
+
+
+def main() -> None:
+    sections = soup.find_all("section")
+    section_dict = {}
+    for section in sections:
+        section_title, collection = process_section(section)
+        # Handle Duplicates
+        if section_title == "Digital & Crypto Currencies":
+            del collection["CURRENCY_EXCHANGE_RATE"]
+        section_dict[section_title] = collection
+
     output_stream = io.StringIO()
 
     original_stdout = sys.stdout
@@ -96,54 +201,7 @@ def write_processed_dict_to_script(section_dict) -> None:
 
     try:
         for section, dict_ in section_dict.items():
-            print("    ", "#" * (len(section) + 4), sep="")
-            print("    ", "# ", section, " #", sep="")
-            print("    ", "#" * (len(section) + 4), sep="")
-
-            print()
-            for k, v in dict_.items():
-                args_req = [a[0] for a in v["args_required"]]
-                args_req_str = ", ".join(args_req)
-                args_req_request = [f'"{arg}={arg}"' for arg in args_req]
-
-                args_opt = [a[0] for a in v["args_optional"]]
-                args_opt_adj = [format_opt_arg(arg) for arg in args_opt]
-                args_opt_str = ", ".join(args_opt_adj)
-                args_opt_request = [format_opt_request_arg(arg) for arg in args_opt]
-
-                args = ["self"]
-                if args_req_str != "":
-                    args.append(args_req_str)
-                if args_opt_str != "":
-                    args.append(args_opt_str)
-                print(
-                    "    ",
-                    f"def get_{k.lower()}({','.join(args)},**kwargs) -> dict[str, any]:",
-                    sep="",
-                )
-                print('        """')
-                print(f"https://www.alphavantage.co/documentation/#{v['id']}")
-                for line in v["description"].splitlines():
-                    print("", line, sep="")
-                for arg, desc in v["args_required"]:
-                    print(f"### {arg} (required)")
-                    print(f"{desc}")
-                for arg, desc in v["args_optional"]:
-                    print(f"### {arg} (optional)")
-                    print(f"{desc}")
-                print('        """')
-                request_args_optional = (
-                    f" + {' + '.join(args_opt_request)}"
-                    if len(args_opt_request) > 0
-                    else ""
-                )
-                print(f"""
-            return self._send_request(
-                function="{k}",
-                request_args=[{','.join(args_req_request)}]{request_args_optional},
-                **kwargs
-            )
-                """)
+            print_section(section, dict_)
     finally:
         sys.stdout = original_stdout
 
@@ -151,26 +209,8 @@ def write_processed_dict_to_script(section_dict) -> None:
 
     with open("util/_av_integration_api_base.py", "r") as file:
         code_base = file.read()
-    with open("test_output.py", "w") as file:
+    with open("testing.py", "w") as file:
         file.write(code_base + "\n" + python_code)
-
-
-def main() -> None:
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise RuntimeError("Failed to get HTML documentation from AlphaVantage!")
-
-    soup = bs4.BeautifulSoup(response.text, "html.parser")
-
-    sections = soup.find_all("section")
-    section_dict = {}
-    for section in sections:
-        section_title, collection = process_section(section)
-        if section_title == "Digital & Crypto Currencies":
-            del collection[
-                "CURRENCY_EXCHANGE_RATE"
-            ]  # This is duplicated, also appears in FX
-        section_dict[section_title] = collection
 
 
 if __name__ == "__main__":
