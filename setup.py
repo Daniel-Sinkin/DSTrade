9672c0cb2db179694d92f483e665b5708ac158c2
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-12-20 13:47:24 +0100

    Setup everything to be able to pip install from repo

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="DSTrade",
+    version="0.1.0",
+    author="Daniel Sinkin",
+    author_email="danielsinkin97@gmail.com",
+    description="Full Alphavantage API python interface",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Daniel-Sinkin/DSTrade",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+    python_requires=">=3.10",
+)
