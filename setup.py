b550ccae63bfd941800eb6b3246f877551de4e5f
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-01-17 14:14:42 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="DSTrade",
-    version="0.1.0",
+    version="0.1.1",
     author="Daniel Sinkin",
     author_email="danielsinkin97@gmail.com",
     description="Full Alphavantage API python interface",
